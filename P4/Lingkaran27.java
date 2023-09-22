faa8e68569a546853ee1a779bfd383859911efc2
Rakai Seto <mamangrakai@gmail.com>
2023-09-22 16:11:20 +0700

    P4_Percobaan 1_Pertanyaan 3_27

--- P4/Lingkaran27.java
+++ P4/Lingkaran27.java
@@ -4,13 +4,13 @@ class Lingkaran27 {
     public static void main(String[] args) {
         int r;
         double keliling, luas;
-
+        double phi = 3.14;
         System.out.println("Masukkan jari-jari lingkaran");
         Scanner sc = new Scanner(System.in);
         r = sc.nextInt();
 
-        keliling = 2 * 3.14 * r;
-        luas = 3.14 * r * r;
+        keliling = 2 * phi * r;
+        luas = phi * r * r;
 
         System.out.println("Keliling : " + keliling);
         System.out.println("Luas : " + luas);
