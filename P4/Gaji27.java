599aaa748a4e2c03f1dc3cc7638996a5e783b1e7
Rakai Seto <mamangrakai@gmail.com>
2023-09-22 16:21:23 +0700

    P4_Percobaan 2_Pertanyaan 2_27

--- P4/Gaji27.java
+++ P4/Gaji27.java
@@ -2,15 +2,17 @@ import java.util.Scanner;
 
 public class Gaji27 {
     public static void main(String[] args) {
-        int jmlMasuk, jmlTidakMasuk, totGaji;
-        int gaji = 40000;
-        int potGaji = 25000;
+        int jmlMasuk, jmlTidakMasuk, totGaji, gaji, potGaji;
 
         Scanner sc = new Scanner(System.in);
         System.out.println("Masukkan jumlah masuk");
         jmlMasuk = sc.nextInt();
         System.out.println("Masukkan jumlah tidak masuk");
         jmlTidakMasuk = sc.nextInt();
+        System.out.println("Masukkan besaran gaji");
+        gaji = sc.nextInt();
+        System.out.println("Masukkan besaran potongan gaji");
+        potGaji = sc.nextInt();
 
         totGaji = (jmlMasuk * gaji) - (jmlTidakMasuk * potGaji);
         System.out.println("Total Gaji : " + totGaji);
