a22e0149feb4dc336455ee0fe3e683335a91ec44
Rakai Seto <mamangrakai@gmail.com>
2023-10-06 14:29:52 +0700

    P5_Percobaan2_Pertanyaan1

--- PemilihanPercobaan2_27.java
+++ PemilihanPercobaan2_27.java
@@ -14,11 +14,28 @@ public class PemilihanPercobaan2_27 {
             float tugas = input27.nextFloat();
 
             float result = (UAS * 0.4F) + (UTS * 0.3F) + (kuis * 0.1F) + (tugas * 0.2F);
+            String huruf = "";
+
+            if (result > 81) {
+                huruf = "A";
+            } else if (result > 73) {
+                huruf = "B+";
+            } else if (result > 65) {
+                huruf = "B";
+            } else if (result > 60) {
+                huruf = "C+";
+            } else if (result > 50) {
+                huruf = "C";
+            } else if (result > 39) {
+                huruf = "D";
+            } else {
+                huruf = "E";
+            }
 
             if (result < 65) {
-                System.out.println("Siswa perlu remidi karena nilai : " + result);
+                System.out.println("Siswa perlu remidi karena nilai : " + result + " dengan huruf nilai " + huruf);
             } else {
-                System.out.println("Siswa tidak perlu remidi karena nilai : " + result);
+                System.out.println("Siswa tidak perlu remidi karena nilai : " + result + " dengan huruf nilai " + huruf);
             }
         } finally {
             input27.close();
