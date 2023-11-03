052796660d9e3b18b82ae4da8250709008a083b9
Rakai Seto <mamangrakai@gmail.com>
2023-11-03 15:35:25 +0700

    P9_Percobaan4_Pertanyaan2

--- P9/LinearSearch27.java
+++ P9/LinearSearch27.java
@@ -1,17 +1,36 @@
+import java.util.Scanner;
+
 public class LinearSearch27 {
     public static void main(String[] args) {
-        int[] arrayInt = {34, 18, 26, 48, 72, 20, 56, 63};
+        Scanner sc27 = new Scanner(System.in);
+        System.out.print("Masukkan jumlah elemen array : ");
 
-        int key = 20;
+        int elemen = sc27.nextInt();
+        int[] arrayInt = new int[elemen];
+
+        for (int i = 0; i < arrayInt.length; i++) {
+            System.out.print("Masukkan elemen ke " + (i+1) + " : ");
+            arrayInt[i] = sc27.nextInt();
+        }
+
+        System.out.print("Masukkan key yang ingin dicari : ");
+
+        int key = sc27.nextInt();
         int hasil = 0;
+        boolean isfound = false;
 
         for (int i = 0; i < arrayInt.length; i++) {
             if (arrayInt[i] == key) {
                 hasil = i;
+                isfound = true;
                 break;
             }
         }
 
-        System.out.println("Key ada dalam array pada posisi index ke-" + hasil);
+        if (isfound) {
+            System.out.println("Key ada dalam array pada posisi index ke-" + hasil);
+        } else {
+            System.out.println("Key tidak ditemukan");
+        }
     }
 }
