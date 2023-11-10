18aff3055d42fd9b2f96645febe1299513fb3cb3
Rakai Seto <mamangrakai@gmail.com>
2023-11-10 16:12:32 +0700

    P10_Percobaan2_Pertanyaan2

--- P10/BioskopWithScanner27.java
+++ P10/BioskopWithScanner27.java
@@ -6,24 +6,52 @@ public class BioskopWithScanner27 {
     public static void main(String[] args) {
         Scanner sc27  = new Scanner(System.in);
         int baris, kolom;
-        String nama, next;
+        String nama, next="y";
+        String menu = "";
 
         String[][] penonton = new String[4][2];
 
-        while (true) {
-            System.out.println("Masukkan nama : ");
-            nama = sc27.nextLine();
-            System.out.println("Masukkan baris : ");
-            baris = sc27.nextInt();
-            System.out.println("Masukkan kolom : ");
-            kolom = sc27.nextInt();
+        for(; ;) {
+            if (menu.equals("pilih") || menu.equals("")) {
+                System.out.println("=========================");
+                System.out.println("Pilih menu :");
+                System.out.println("1. Input Data Penonton");
+                System.out.println("2. Tampilkan Daftar Penonton");
+                System.out.println("3. Exit");
+                System.out.print("menu : ");
+                menu = sc27.next();
+            } else if (menu.equals("1")) {
+                System.out.println();
+                next = "y";
+                while (next.equals("y")) {
+                    System.out.print("Masukkan nama : ");
+                    nama = sc27.next();
+                    System.out.print("Masukkan baris : ");
+                    baris = sc27.nextInt();
+                    System.out.print("Masukkan kolom : ");
+                    kolom = sc27.nextInt();
 
-            penonton[baris-1][kolom-1] = nama;
+                    penonton[baris-1][kolom-1] = nama;
 
-            System.out.println("Input penonton lainnya? (Y/N)");
-            next = sc27.nextLine();
-            if (next.equalsIgnoreCase("n")) {
+                    System.out.print("Input penonton lainnya? (Y/N) : ");
+                    next = sc27.next();
+                    if (next.equalsIgnoreCase("n")) {
+                        menu = "";
+                        continue;
+                    }
+                }
+            } else if (menu.equals("2")) {
+                System.out.println();
+                for (int i = 0; i < penonton.length; i++) {
+                    System.out.println(String.join(", ", penonton[i]));
+                }
+                menu = "";
+                continue;
+            } else if (menu.equals("3")) {
                 break;
+            } else {
+                menu = "";
+                continue;
             }
         }
     }
