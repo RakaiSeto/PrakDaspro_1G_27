cf8a1394a69ffadecc6d7e211393a133d60fe794
Rakai Seto <mamangrakai@gmail.com>
2023-11-10 14:05:02 +0700

    P10_Percobaan1

---
+++ P10/Bioskop27.java
@@ -0,0 +1,21 @@
+package P10;
+
+public class Bioskop27 {
+    public static void main(String[] args) {
+        String[][] penonton = new String[4][2];
+
+        penonton[0][0] = "Rakai";
+        penonton[0][1] = "Linlin";
+        penonton[1][0] = "Dina";
+        penonton[1][1] = "Fabian";
+        penonton[2][0] = "Yefta";
+        penonton[2][1] = "Adam";
+        penonton[3][0] = "Fandy";
+        penonton[3][1] = "Adani";
+
+        System.out.printf("%s \t %s\n", penonton[0][0], penonton[0][1]);
+        System.out.printf("%s \t %s\n", penonton[1][0], penonton[1][1]);
+        System.out.printf("%s \t %s\n", penonton[2][0], penonton[2][1]);
+        System.out.printf("%s \t %s\n", penonton[3][0], penonton[3][1]);
+    }
+}
