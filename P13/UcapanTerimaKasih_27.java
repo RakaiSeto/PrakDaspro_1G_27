0e11520ad7957552203ae03e3e1c242dc8b53baa
Rakai Seto <mamangrakai@gmail.com>
2023-12-01 14:07:42 +0700

    P13_Percobaan4_Pertanyaan3

--- P13/UcapanTerimaKasih_27.java
+++ P13/UcapanTerimaKasih_27.java
@@ -10,6 +10,8 @@ public class UcapanTerimaKasih_27 {
     public static void UcapanTerimaKasih() {
         String nama = PenerimaUcapan();
         System.out.println("Thank you "+ nama + "for being the best teacher in the world.\n" + "You inspired in me a love for learning and made me feel like i can ask you anything");
+        String ucapan = "Terima Kasih Pak.. Bu.. Semoga Sehat Selalu";
+        UcapanTambahan(ucapan);
     }
 
     public static String PenerimaUcapan() {
@@ -18,5 +20,9 @@ public class UcapanTerimaKasih_27 {
         String namaOrang = sc27.nextLine();
         sc27.close();
         return namaOrang;
+    }
+
+    public static void UcapanTambahan(String ucapan) {
+        System.out.println(ucapan);
     }
 }
