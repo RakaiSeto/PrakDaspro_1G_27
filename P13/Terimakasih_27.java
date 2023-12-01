6ee9ca7bae5aeb6cd5979c607eb4d8a1a6f84390
Rakai Seto <mamangrakai@gmail.com>
2023-12-01 13:33:52 +0700

    P13_Percobaan1

---
+++ P13/Terimakasih_27.java
@@ -0,0 +1,11 @@
+package P13;
+
+public class Terimakasih_27 {
+    public static void main(String[] args) {
+        UcapanTerimaKasih();
+    }
+
+    public static void UcapanTerimaKasih() {
+        System.out.println("Thank you for being the best teacher in the world.\n" + "You inspired in me a love for learning and made me feel like i can ask you anything");
+    }
+}
