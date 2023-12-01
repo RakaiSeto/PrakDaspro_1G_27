82ffd559dfc8b4bc81fa9419fa9ec90e5f356304
Rakai Seto <mamangrakai@gmail.com>
2023-12-01 13:51:50 +0700

    P13_percobaan4

---
+++ P13/UcapanTerimaKasih_27.java
@@ -0,0 +1,22 @@
+package P13;
+
+import java.util.Scanner;
+
+public class UcapanTerimaKasih_27 {
+    public static void main(String[] args) {
+        UcapanTerimaKasih();
+    }
+
+    public static void UcapanTerimaKasih() {
+        String nama = PenerimaUcapan();
+        System.out.println("Thank you "+ nama + "for being the best teacher in the world.\n" + "You inspired in me a love for learning and made me feel like i can ask you anything");
+    }
+
+    public static String PenerimaUcapan() {
+        Scanner sc27 = new Scanner(System.in);
+        System.out.println("Tuliskan NAMA orang yang ingin anda beri ucapan");
+        String namaOrang = sc27.nextLine();
+        sc27.close();
+        return namaOrang;
+    }
+}
