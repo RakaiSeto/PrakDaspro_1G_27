bed953bad9c6288593c3882017366c132a238f57
Rakai Seto <mamangrakai@gmail.com>
2023-12-01 13:40:36 +0700

    P13_Percobaan2

--- P13/Terimakasih_27.java
+++ P13/Terimakasih_27.java
@@ -2,11 +2,16 @@ package P13;
 
 public class Terimakasih_27 {
     public static void main(String[] args) {
-        System.out.println("Thank you for being the best teacher in the world.\n" + "You inspired in me a love for learning and made me feel like i can ask you anything");
         UcapanTerimaKasih();
+        String ucapan = "Terima Kasih Pak.. Bu.. Semoga Sehat Selalu";
+        UcapanTambahan(ucapan);
     }
 
     public static void UcapanTerimaKasih() {
         System.out.println("Thank you for being the best teacher in the world.\n" + "You inspired in me a love for learning and made me feel like i can ask you anything");
+    }
+
+    public static void UcapanTambahan(String ucapan) {
+        System.out.println(ucapan);
     }
 }
